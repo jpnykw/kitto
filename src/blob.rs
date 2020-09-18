c37511d0af756484bbf657011711c8ab9fa18c9c
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-09-19 00:14:20 +0900

    :sparkles: Implement blob decoder

--- src/blob.rs
+++ src/blob.rs
@@ -1,8 +1,22 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Blob(pub String);
 
-pub fn encode(text: String) -> Option<Blob> {
+pub fn encode(text: &String) -> Option<Blob> {
   let length = text.chars().count();
   Some(Blob(String::from(format!("blob {}\0{}", length, text))))
+}
+
+pub fn decode(object: Blob) -> Option<String> {
+  match object {
+    Blob(text) => {
+      // ヌル文字で区切って
+      let mut null_split = text.split("\0").collect::<Vec<&str>>();
+      // ヘッダー部分を除去
+      null_split.remove(0);
+      // コンテンツにヌル文字が含まれていることを考慮して
+      // 後ろの配列を結合して文字列に変換
+      Some(null_split.join("\0"))
+    }
+  }
 }
 
